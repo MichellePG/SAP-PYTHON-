24080552a1adebfde0935424ad3bd12f47b1e273
MichellePG <89443528+MichellePG@users.noreply.github.com>
2023-03-03 03:06:38 -0300

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -8,11 +8,12 @@
 #details of the implementation.
 
 
-#Example 1: E2E_TESTING_AGENT
+#Example 1: Function E2E_TESTING_AGENT (performs end-to-end (E2E) testing of SAP systems)
 
 from pyrfc import Connection
 
-conn = Connection(user='username', passwd='password', ashost='sapserver', sysnr='00', client='x00')
+
+conn = Connection(user='username', passwd='password', ashost='host', sysnr='00', client='x00')
 
 # Set up the input parameters for the E2E_TESTING_AGENT function
 params = {
@@ -49,8 +50,88 @@ print(result)
 
 
 
-#Example 2: 
+#Example 2: Function BAPI_MATERIAL_AVAILABILITY (retrieves the stock and availability information for a material)
+
+import pyrfc
+
+# Create a connection to SAP system
+conn = pyrfc.Connection(user='user', passwd='password', ashost='host', sysnr='00', client='x00')
+
+# Define input parameters for transaction
+input_params = {
+    'IV_MATNR': '000000000000010000',  # Material number
+    'IV_WERKS': '1000',                # Plant
+    'IV_LGORT': '0001'                 # Storage location
+}
+
+# Call transaction to execute
+result = conn.call('BAPI_MATERIAL_AVAILABILITY', **input_params)
+
+# Display output
+print(result['ET_ATP_DATA'])
+
+#In this example, we first create a connection to the SAP system using the pyrfc.Connection method. 
+#We then define the input parameters for the transaction in a dictionary, where the keys correspond to the 
+#input parameters expected by the transaction.
+
+#We then call the transaction using the conn.call method, passing in the name of the transaction and the 
+#input parameters as keyword arguments. The method returns a dictionary containing the output parameters from 
+#the transaction.
+
+#In this example, we're calling the BAPI_MATERIAL_AVAILABILITY transaction to check the availability of a material
+#at a specific plant and storage location. We're then printing the contents of the ET_ATP_DATA output parameter, which 
+#contains the availability data for the material.
 
 
 
+#Example 3: Function BAPI_FLIGHT_GETLIST (retrieves a list of flights based on various searching criteria)
+
+import pyrfc
+
+# Connection parameters
+conn_params = {"user": "username","passwd": "password","ashost": "sap_host","sysnr": "00","client": "100","lang": "EN",}
+
+# Initialize the connection
+conn = pyrfc.Connection(**conn_params)
+
+# Call the BAPI_FLIGHT_GETLIST function module
+flight_list = conn.call("BAPI_FLIGHT_GETLIST", AIRLINECARRIER="LH")
+
+# Print the result
+print(flight_list)
+
+#This code connects to a SAP system using the specified connection parameters, and then calls the BAPI_FLIGHT_GETLIST 
+#function module with the AIRLINECARRIER parameter set to "LH". The result is then printed to the console. Note that 
+#you'll need to replace the connection parameters with values that are appropriate for your SAP system.
+
+
+
+#Example 4: Function BAPI_BUSPARTNER_GETDETAIL (retrieves the details of a business partner)
+
+import pyrfc
+
+# create a connection to SAP system
+conn = pyrfc.Connection(user='username', passwd='password', ashost='host', sysnr='00', client='x00')
+
+# create a dictionary with input parameters for the BAPI
+bapi_input = {
+    'BUSINESSPARTNERID': '12345',
+    'PARTNERCATEGORY': 'ABC',
+    'LANGUAGE': 'EN'
+}
+
+# call the BAPI using the 'BAPI_BUSPARTNER_GETDETAIL' function module
+result = conn.call('BAPI_BUSPARTNER_GETDETAIL', **bapi_input)
+
+# check the result
+if result['RETURN'][0]['TYPE'] == 'E':
+    print('Error occurred: ', result['RETURN'][0]['MESSAGE'])
+else:
+    print('Business partner details: ', result['BUSINESSPARTNER'])
+
+#In this example, we first create a connection to the SAP system using the pyrfc.Connection() method. We then 
+#define a dictionary bapi_input with input parameters for the BAPI_BUSPARTNER_GETDETAIL function module. We call the 
+#BAPI using the conn.call() method and pass the BAPI name and input parameters. The result variable will contain the
+#output of the BAPI. We then check the result variable for any errors and print the business partner details if the 
+#call was successful.
 
